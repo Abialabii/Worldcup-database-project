782ea6752522e99b81aef3f99fd4c555e961a614
Abialabii <168450308+Abialabii@users.noreply.github.com>
2024-08-30 12:24:29 +0100

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -8,7 +8,7 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-echo &($PSQL "TRUNCATE teams")
+echo $($PSQL "TRUNCATE teams, games")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do 
@@ -19,19 +19,19 @@ do
     #if not found
     if [[ -z $WINNER_ID ]]
     then
-      $($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
+      echo $($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
     fi
 
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
    #if not found
     if [[ -z $OPPONENT_ID ]]
     then 
-      $($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
+      echo $($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
     fi
 
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
-    $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals,opponent_goals) VALUES ($YEAR,'$ROUND',$WINNER_ID,$OPPONENT_ID,$WINNER_GOALS,$OPPONENT_GOALS)")
+   echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals,opponent_goals) VALUES ($YEAR,'$ROUND',$WINNER_ID,$OPPONENT_ID,$WINNER_GOALS,$OPPONENT_GOALS)")
   fi
   
 done
